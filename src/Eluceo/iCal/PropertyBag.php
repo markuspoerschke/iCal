3cd465da5d87366f056bc7d27227136cfdf869bd
Markus Poerschke <markus@eluceo.de>
2013-02-20 20:33:10 +0100

    Added license

--- src/Eluceo/iCal/PropertyBag.php
+++ src/Eluceo/iCal/PropertyBag.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the eluceo/iCal package.
+ *
+ * (c) Markus Poerschke <markus@eluceo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Eluceo\iCal;
 
 use Eluceo\iCal\Property;
