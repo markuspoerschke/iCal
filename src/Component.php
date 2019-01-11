8d691532e39afcf779d3b9a00a2f1084a0a6f79c
Julien Bourdeau <julien@sigerr.org>
2019-01-11 16:53:33 +0100

    Introduce Component::setComponents method (#124)
    * Introduce Component::setComponents method
    
    * Better param description for setComponents in phpdoc
    
    Co-Authored-By: julienbourdeau <julien@sigerr.org>

--- src/Component.php
+++ src/Component.php
@@ -70,6 +70,19 @@ abstract class Component
     }
 
     /**
+     * Set all Components.
+     *
+     * @param Component[] $components The array of Component that will be set
+     * @param null        $key        The key of the Component
+     */
+    public function setComponents(array $components)
+    {
+        $this->components = $components;
+
+        return $this;
+    }
+
+    /**
      * Renders an array containing the lines of the iCal file.
      *
      * @return array
