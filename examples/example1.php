7f97f190b61a695d2963ec6db6e6c762b5ab1c54
Markus Poerschke <markus@poerschke.nrw>
2021-04-05 00:30:17 +0200

    Update x-apple-structured-location documentation (#236)

--- examples/example1.php
+++ examples/example1.php
@@ -18,6 +18,8 @@ use Eluceo\iCal\Domain\Entity\Event;
 use Eluceo\iCal\Domain\ValueObject\Alarm;
 use Eluceo\iCal\Domain\ValueObject\Attachment;
 use Eluceo\iCal\Domain\ValueObject\DateTime;
+use Eluceo\iCal\Domain\ValueObject\GeographicPosition;
+use Eluceo\iCal\Domain\ValueObject\Location;
 use Eluceo\iCal\Domain\ValueObject\TimeSpan;
 use Eluceo\iCal\Domain\ValueObject\Uri;
 use Eluceo\iCal\Presentation\Factory\CalendarFactory;
@@ -29,6 +31,10 @@ $event = new Event();
 $event
     ->setSummary('Christmas Eve')
     ->setDescription('Lorem Ipsum Dolor...')
+    ->setLocation(
+        (new Location('Neuschwansteinstraße 20, 87645 Schwangau', 'Schloss Neuschwanstein'))
+            ->withGeographicPosition(new GeographicPosition(47.557579, 10.749704))
+    )
     ->setOccurrence(
         new TimeSpan(
             new DateTime(DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2030-12-24 13:30:00'), true),
